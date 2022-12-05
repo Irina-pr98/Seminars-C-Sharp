120fe7b180e3f901b48c2d3bfaea1209b78be398
Irina <ms.irina-98@mail.ru>
2022-12-05 14:36:25 +0300

    add comment to Task 1

--- Seminar 1/Task 1/Program.cs
+++ Seminar 1/Task 1/Program.cs
@@ -7,4 +7,4 @@ Console.Clear();
 
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Результат: {n * n}");
+Console.WriteLine($"Результат: {n * n}"); // $ - смешение строки и числа
