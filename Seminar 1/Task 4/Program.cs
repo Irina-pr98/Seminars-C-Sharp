672b82ad57f2a1daa133b99c0dd83b135388126b
Irina <ms.irina-98@mail.ru>
2022-12-05 16:38:16 +0300

    change Task 4

--- Seminar 1/Task 4/Program.cs
+++ Seminar 1/Task 4/Program.cs
@@ -2,7 +2,7 @@
     4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
     2 -> "-2, -1, 0, 1, 2" */
 
-    Console.Clear();
+Console.Clear();
 
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
