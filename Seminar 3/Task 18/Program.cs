8b457c8888f596428fa2e4296df975d79b3e96cd
Irina <ms.irina-98@mail.ru>
2022-12-08 21:42:52 +0300

    add Task 18

---
+++ Seminar 3/Task 18/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая по заданному номеру четверти, показывает диапазон возможныхкоординат точек
+    в этой четверти (x и y) */
+
+Console.Clear();
+
+Console.Write("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 1 || n == 2)
+    Console.WriteLine("y > 0");
+else
+    Console.WriteLine("y < 0");
+
+if (n == 1 || n == 4)
+    Console.WriteLine("x > 0");
+else
+    Console.WriteLine("x < 0");
